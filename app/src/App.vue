36a667baede62fe878995313529c38fef7d292bb
rg3915 <regis42santos@gmail.com>
2022-11-08 03:58:34 -0300

    v-bind

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,11 +2,17 @@
     <div>
         <div
             v-for="(obj, index) in todos"
-            v-bind:key="obj.id"
+            :key="obj.id"
             class="todos-item"
         >
-            {{ index}} - {{ obj.title }}
+            <img
+                v-if="obj.imgSrc"
+                :src="obj.imgSrc"
+            >
+            {{ index }} - {{ obj.title }}
         </div>
+
+
     </div>
 </template>
 
@@ -15,17 +21,20 @@ export default {
     name: 'App',
     data() {
         return {
-            todos: [{
+            todos: [
+                {
                     "userId": 1,
                     "id": 1,
                     "title": "delectus aut autem",
-                    "completed": false
+                    "completed": false,
+                    "imgSrc": 'https://via.placeholder.com/150',
                 },
                 {
                     "userId": 1,
                     "id": 2,
                     "title": "quis ut nam facilis et officia qui",
-                    "completed": false
+                    "completed": false,
+                    "imgSrc": 'https://via.placeholder.com/150',
                 },
                 {
                     "userId": 1,
@@ -58,7 +67,6 @@ export default {
     padding: 3px 6px;
     color: #fff
 }
-
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
