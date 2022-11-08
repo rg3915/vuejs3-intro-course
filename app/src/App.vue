603590621c46f0aa7485467c233277698199e581
rg3915 <regis42santos@gmail.com>
2022-11-08 03:56:14 -0300

    v-if

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,37 @@
 <template>
-  <TheHeader />
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheHeader v-if="showHeader" />
+
+    <br>
+
+    <tab>tab 1</tab>
+    <tab>tab 2</tab>
+
+    <div>
+      Conteúdo 1
+    </div>
+
+    <div>
+      Conteúdo 2
+    </div>
+
+    <br>
+
+    <div v-show="showName">
+      Nome: {{ firstName }} <br>
+      Sobrenome: {{ lastName }}
+    </div>
+
+    <br>
+
+    <div v-if="accessLevel === 'admin'">Usuário Admin</div>
+    <div v-else-if="accessLevel === 'marketing'">Usuário Marketing</div>
+    <div v-else>User</div>
+
+    <img
+        alt="Vue logo"
+        src="./assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
@@ -9,21 +39,30 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheHeader from './components/TheHeader.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    TheHeader
-  }
+    name: 'App',
+    components: {
+        HelloWorld,
+        TheHeader
+    },
+    data() {
+        return {
+            showHeader: true,
+            firstName: 'Evan',
+            lastName:  'You',
+            showName: true,
+            accessLevel: 'admin'
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
