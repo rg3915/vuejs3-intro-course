68261c389a6d8d322989acf09e63db2f846820eb
rg3915 <regis42santos@gmail.com>
2022-11-08 04:15:08 -0300

    componente slot

--- app/src/components/TheHeader.vue
+++ app/src/components/TheHeader.vue
@@ -1,12 +1,26 @@
 <template>
     <header class="header">
-        Header
+        <h1
+            v-if="$slots.title"
+            class="title">
+            <slot name="title" />
+        </h1>
+
+        <div class="description">
+            <slot name="description" />
+        </div>
+
+        <div class="content">
+            <slot />
+        </div>
     </header>
 </template>
 
 <script>
     export default {
-
+        mounted() {
+            console.log(this.$slots);
+        }
     }
 </script>
 
