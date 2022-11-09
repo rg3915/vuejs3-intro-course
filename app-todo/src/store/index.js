3e789e38fe749abf8f5d7eccf22570a3ec526640
rg3915 <regis42santos@gmail.com>
2022-11-09 05:39:41 -0300

    Deletando um item

--- app-todo/src/store/index.js
+++ app-todo/src/store/index.js
@@ -17,6 +17,13 @@ export default createStore({
         state.todos.push(payload)
       }
     },
+    deleteTodo(state, id) {
+      const index = state.todos.findIndex(todo => todo.id === id)
+
+      if (index >= 0) {
+        state.todos.splice(index, 1)
+      }
+    },
   },
   actions: {
     getTodos({ commit }) {
@@ -38,6 +45,11 @@ export default createStore({
     updateTodo({ commit }, { id, data }) {
       return axios.put(`http://localhost:3000/todos/${id}`, data).then((response) => {
         commit('storeTodo', response.data)
+      })
+    },
+    deleteTodo({ commit }, id) {
+      return axios.delete(`http://localhost:3000/todos/${id}`).then(() => {
+        commit('deleteTodo', id)
       })
     }
   },
