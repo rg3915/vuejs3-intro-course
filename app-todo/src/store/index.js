1a93321cacc4cc79f8166a247f12646952f395d1
rg3915 <regis42santos@gmail.com>
2022-11-09 05:25:37 -0300

    Editando um item

--- app-todo/src/store/index.js
+++ app-todo/src/store/index.js
@@ -25,11 +25,13 @@ export default createStore({
         }, 1000)
       })
     },
-
     addTodo({ commit }, data) {
       return axios.post('http://localhost:3000/todos', data).then((response) => {
         commit('storeTodo', response.data);
       })
+    },
+    updateTodo(context, { id, data }) {
+      return axios.put(`http://localhost:3000/todos/${id}`, data)
     }
   },
   getters: {
